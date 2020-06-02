92bf6a9c8c8e17c08bb338278bdcad5bc46d2bd8
Shailesh Borate <shaileshborate@gmail.com>
2020-06-02 16:08:59 +0530

    Updates

---
+++ precache-manifest.72ab5892ba0222e0f507276f9b0bdd1c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cc3062f1e48f6fdc260abaa5a81f71",
+    "url": "/PuneCovid19/index.html"
+  },
+  {
+    "revision": "af32d16302efcfcd7e76",
+    "url": "/PuneCovid19/static/css/2.f7bd06e3.chunk.css"
+  },
+  {
+    "revision": "85abfff371144a635fad",
+    "url": "/PuneCovid19/static/css/main.3c15c924.chunk.css"
+  },
+  {
+    "revision": "af32d16302efcfcd7e76",
+    "url": "/PuneCovid19/static/js/2.27c6734e.chunk.js"
+  },
+  {
+    "revision": "b7b068e7a9ff7c79ac98ce106091f1d8",
+    "url": "/PuneCovid19/static/js/2.27c6734e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85abfff371144a635fad",
+    "url": "/PuneCovid19/static/js/main.f681a33c.chunk.js"
+  },
+  {
+    "revision": "7ff931b7fde545208d4b",
+    "url": "/PuneCovid19/static/js/runtime-main.cad285ba.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/PuneCovid19/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/PuneCovid19/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/PuneCovid19/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/PuneCovid19/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/PuneCovid19/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
